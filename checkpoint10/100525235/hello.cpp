96138aa83dafbcc857fcf58d4fd020205296eb6a
Miral Panchal <miral.panchal@uoit.net>
2015-10-05 18:41:34 -0400

    Miral Panchal

--- checkpoint10/100525235/hello.cpp
+++ checkpoint10/100525235/hello.cpp
@@ -4,7 +4,7 @@ int main()
 {
         // only by replacing missing condition in if statement
         // marked by (...), bring output as "Hello world!"
-	if (true)
+	if(! (std::cout<< "Hello ") )
 		std::cout << "Hello ";
 	else
 		std::cout << "world!\n";
