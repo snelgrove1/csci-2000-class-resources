302ef4546de6da1bfdc581bfecd2c5b36a64f2b1
Hyo Kim <hyo.kim@uoit.net>
2015-10-05 18:48:28 -0400

    Hyo Kim

--- checkpoint10/hello.cpp
+++ checkpoint10/hello.cpp
@@ -4,7 +4,7 @@ int main()
 {
         // only by replacing missing condition in if statement
         // marked by (...), bring output as "Hello world!"
-	if(! (std::cout<< "Hello ") )
+	if(true)
 		std::cout << "Hello ";
 	else
 		std::cout << "world!\n";
