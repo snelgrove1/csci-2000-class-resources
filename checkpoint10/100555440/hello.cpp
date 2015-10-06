ec39644c69c5e6298adbb75c262593ada6fb1ffb
Saif Niaz <saifniaz.3@gmail.com>
2015-10-05 22:58:01 -0400

    MD. Saif Niaz

---
+++ checkpoint10/100555440/hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+        // only by replacing missing condition in if statement
+        // marked by (...), bring output as "Hello world!"
+	if (! (std::cout << "Hello ")){
+		//std::cout << "Hello ";
+	}else{
+		std::cout << "world!\n";
+	}
+}
