969c86158a3809aede53d800a4fac066d62dc96c
Austin Oligario <austinoligario@gmail.com>
2015-10-07 09:11:00 -0400

    Ausitn Oligario

--- checkpoint9/100554418/hello.c
+++ checkpoint9/100554418/hello.c
@@ -1,13 +1,8 @@
-#include <stdio.h>
+Merge branch 'master' of https://github.com/arminms/CSCI-2000U
 
-int main()
-{
-	/* only by replacing missing condition in if statement 
- *            marked by (...), bring output as "Hello world!" */ 
-	if (!printf("Hello "))
-		printf("Hello ");
-	else
-		printf("world!\n");
-
-	return 0;
-}
+# Please enter a commit message to explain why this merge is necessary,
+# especially if it merges an updated upstream into a topic branch.
+#
+# Lines starting with '#' will be ignored, and an empty message aborts
+# the commit.
+"Austin Oligario"
