8b76edcd31ba7b96a38f0e4b899fa4b4c6a04797
Austin Oligario <austinoligario@gmail.com>
2015-10-07 08:58:38 -0400

    Ausitn Oligario

--- checkpoint9/100549940/hello.c
+++ checkpoint9/100549940/hello.c
@@ -4,11 +4,7 @@ int main()
 {
 	/* only by replacing missing condition in if statement 
            marked by (...), bring output as "Hello world!" */ 
-<<<<<<< HEAD:checkpoint9/hello.c
-	if (!printf("Hello") == 0)
-=======
-	if (fork())
->>>>>>> df6885ab045474a7ee755925a8d11063b76b0012:checkpoint9/100558261/hello.c
+	if (printf("Hello ") == 0)
 		printf("Hello ");
 	else
 		printf("world!\n");
