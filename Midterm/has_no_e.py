d878daecd0499910923a819d7f39d14d3d64d5c0
Tyler <tyler.snelgrove@uoit.net>
2015-10-28 09:40:11 -0400

    Midterm: completed has_no_e./py (as much as possible)

---
+++ Midterm/has_no_e.py
@@ -0,0 +1,13 @@
+def has_no_e():
+	for *gadsby.txt
+		if line has 'e'
+			return FALSE
+		else:
+			return TRUE
+
+reader = open('gadsy_stripped.txt','r')
+line = reader.readline()
+while line != ''
+	do has_no_e()
+	line = reader.readling()
+reader.close()
